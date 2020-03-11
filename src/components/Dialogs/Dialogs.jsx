a60452576605e6fe4b03b954410f5157716cc0b0
Artem <artemkkharitonov@yandex.ru>
2020-03-11 14:03:29 +0300

    refactoring navbar and dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,58 @@
 import React from 'react';
 import s from './Dialogs.module.scss';
+import { NavLink } from 'react-router-dom';
+
+const DialogItem = (props) => {
+  return (
+    <div className={s.name}>
+      <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+    </div>
+  );
+}
+
+const Message = (props) => {
+  return (
+    <div className={s.message}>
+      <div className={s.user_info}>
+        <div className={s.image}>
+          <img src={"" + props.img} alt="" />
+        </div>
+        <div className={s.name}>
+          {props.name}
+        </div>
+      </div>
+      <div className={s.user_message}>
+        {props.message}
+      </div>
+    </div>
+  );
+}
 
 const Dialogs = (props) => {
-    return (
-        <div className={s.wrap}>
-            dialogs
+
+
+  return (
+    <div className={s.dialogs}>
+      <h1>
+        Dialogs
+            </h1>
+      <div className={s.dialogs_inner}>
+        <div className={s.user_item}>
+          <DialogItem name='Andrew' id='1' />
+          <DialogItem name='Sasha' id='2' />
+          <DialogItem name='Sveta' id='3' />
+          <DialogItem name='Valera' id='4' />
+          <DialogItem name='Viktor' id='5' />
+          <DialogItem name='Petr' id='6' />
+          <DialogItem name='Alex' id='7' />
         </div>
-    );
+        <div className={s.messages}>
+          <Message name='Andrew' img='https://ufolabs.net/data/attachments/0/261-ed89b82f7ab98db17d82ee9e15195971.jpg' message='Hohoohhoh' />
+          <Message name='Me' img='https://ufolabs.net/data/attachments/0/261-ed89b82f7ab98db17d82ee9e15195971.jpg' message='Kokokkokko' />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dialogs;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import s from './Navbar.module.scss';
+import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.inner}>
-        <a href="/profile">Profile</a>
-        <a href="/dialogs">Messages</a>
-        <a href="/news">News</a>
-        <a href="/music">Music</a>
-        <a href="/settings">Settings</a>
+        <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
+        <NavLink to="/dialogs" activeClassName={s.active}>Messages</NavLink>
+        <NavLink to="/news" activeClassName={s.active}>News</NavLink>
+        <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
+        <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
       </div>
     </nav>
   );
