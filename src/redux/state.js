9dd2edb1678610a09f7478c6df38865335525aa3
Artkh <artemkkharitonov@yandex.ru>
2020-03-14 15:12:25 +0300

    import data from index.js in state.js

---
+++ src/redux/state.js
@@ -0,0 +1,23 @@
+let state = {
+  profilePage: {
+    postsData: [
+      { id: 1, message: 'Hey, what are you doing?', like: 10 },
+      { id: 2, message: 'Hello World!', like: 20 },
+    ],
+  },
+  dialogsPage: {
+    dialogsData: [
+      { id: 1, name: 'Andrew' },
+      { id: 2, name: 'Sasha' },
+      { id: 3, name: 'Sveta' },
+      { id: 4, name: 'Valera' },
+      { id: 5, name: 'Viktor' },
+    ],
+    messagesData: [
+      { id: 1, name: 'Andrew', img: 'https://ufolabs.net/data/attachments/0/261-ed89b82f7ab98db17d82ee9e15195971.jpg', message: 'Hohoohhoh' },
+      { id: 2, name: 'Me', img: 'https://ufolabs.net/data/attachments/0/261-ed89b82f7ab98db17d82ee9e15195971.jpg', message: 'Kokokkokko' }
+    ],
+  },
+};
+
+export default state;
